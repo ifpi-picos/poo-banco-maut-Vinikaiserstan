af27cee61bb1d4c6a5939f32107e9424fc154117
Vinicius Neiva <96322305+Vinikaiserstan@users.noreply.github.com>
2023-10-25 19:18:23 -0300

    Update Conta.java

--- banco maut completo/src/bancomaut/Conta.java
+++ banco maut completo/src/bancomaut/Conta.java
@@ -1,16 +1,20 @@
 package bancomaut;
+import java.util.ArrayList;
+import java.util.Date;
 
-public class Conta implements Notificacao{
+public class Conta implements Notificacao {
     private int agencia;
     private int numero;
-    private double saldo;
+    public double saldo;
     private Cliente cliente;
+    private ArrayList<Transacao> transacoes;
 
     public Conta(int agencia, int numero, double saldo, Cliente cliente) {
         this.agencia = agencia;
         this.numero = numero;
         this.saldo = saldo;
         this.cliente = cliente;
+        this.transacoes = new ArrayList<>();
     }
 
     public double getSaldo() {
@@ -19,23 +23,38 @@ public class Conta implements Notificacao{
 
     public void deposita(double valor) {
         saldo += valor;
+        registrarTransacao("Depósito", valor);
         enviarNotificacao("Depósito", valor);
     }
 
     public double saca(double valor) {
         if (saldo >= valor) {
             saldo -= valor;
+            registrarTransacao("Saque", valor);
             enviarNotificacao("Saque", valor);
             return valor;
         } else {
-            return 0; 
+            return 0;
         }
     }
 
     public void transfere(double valor, Conta contaDestino) {
         if (saca(valor) > 0) {
             contaDestino.deposita(valor);
+            registrarTransacao("Transferência", valor);
             enviarNotificacao("Transferência", valor);
+        }
+    }
+
+    private void registrarTransacao(String tipo, double valor) {
+        Transacao transacao = new Transacao(new Date(), tipo, valor);
+        transacoes.add(transacao);
+    }
+
+    public void exibirExtrato() {
+        System.out.println("Extrato da conta " + agencia + "-" + numero + " para " + cliente.getNome());
+        for (Transacao transacao : transacoes) {
+            System.out.println(transacao.getData() + " - " + transacao.getTipo() + " - Valor: " + transacao.getValor());
         }
     }
 
