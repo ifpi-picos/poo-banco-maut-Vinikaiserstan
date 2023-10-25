239eba94e818acc1b2cb0aec622b9b351aadccd5
Vinicius Neiva <96322305+Vinikaiserstan@users.noreply.github.com>
2023-10-25 19:24:41 -0300

    Update Main.java

--- banco maut completo/src/Main.java
+++ banco maut completo/src/Main.java
@@ -2,21 +2,29 @@ import bancomaut.Cliente;
 import bancomaut.ContaCorrente;
 import bancomaut.ContaPoupanca;
 import bancomaut.Endereco;
+import bancomaut.Conta;
 
 import java.util.Date;
+
 public class Main {
     public static void main(String[] args) {
-        Endereco endereco = new Endereco("Rua ABC", 123, "Bairro A", "Cidade B", "UF");
+        Endereco endereco = new Endereco("Rua Vou Surta Com Esse Trabalho", 123, "Bairro A", "Cidade B", "UF");
         Cliente cliente = new Cliente("12345678901", "Vinicius Neiva", new Date(), endereco);
 
         ContaCorrente contaCorrente = new ContaCorrente(1, 101, 1000.0, cliente, 500.0);
-        ContaPoupanca contaPoupanca = new ContaPoupanca(2, 201, 5000.0, cliente, 0.10);
+        ContaPoupanca contaPoupanca = new ContaPoupanca(2, 202, 1500.0, cliente, 0.10);
 
-        contaCorrente.transfere(200.0, contaPoupanca);
-        contaPoupanca.saca(300.0);
-        contaCorrente.deposita(100.0);
+        contaCorrente.deposita(500.0);
+        contaCorrente.saca(200.0);
+        contaCorrente.transfere(300.0, contaPoupanca);
 
-        System.out.println("Saldo da conta corrente: " + contaCorrente.getSaldo());
-        System.out.println("Saldo da conta poupança: " + contaPoupanca.getSaldo());
+        contaPoupanca.deposita(1000.0);
+        contaPoupanca.saca(50.0);
+
+        System.out.println("Extrato da Conta Corrente:");
+        contaCorrente.exibirExtrato();
+
+        System.out.println("Extrato da Conta Poupança:");
+        contaPoupanca.exibirExtrato();
     }
 }
